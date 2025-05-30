ab3681f117613f20405a1459d0e08edf7c9eb44d
Rukshik Nelluri <115903192+rukshik@users.noreply.github.com>
2025-05-30 01:15:00 -0400

    remove openrecent intialize

--- src/main/java/net/imagej/openrecent/OpenRecentCommand.java
+++ src/main/java/net/imagej/openrecent/OpenRecentCommand.java
@@ -10,7 +10,7 @@ import java.net.URL;
 /*
  * Add Open Recent menu and load it from disk, if any exist from previous sessions
  */
-@Plugin(type = Command.class, menuPath = "File>Open Recent", headless = false, initializer = "initialize")
+@Plugin(type = Command.class, menuPath = "File>Open Recent", headless = false)
 public class OpenRecentCommand implements Command {
 
     @Parameter
@@ -20,12 +20,7 @@ public class OpenRecentCommand implements Command {
     private RecentList recentList;
 
     /* get recent list from file and add to the menu */
-    public RecentList initialize() {
-        recentList = RecentList.getInstance();
-        URL iconURL = getClass().getResource("/icons/puzzle-22.png");
-        RecentMenuUpdater.updateRecentMenu(uiService, recentList, iconURL);
-        return recentList;
-    }
+ 
 
     @Override
     public void run() {
